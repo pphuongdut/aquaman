5fef58253f1f400aaee741ea30b407b42d9824b7
Phuong Tran <tranphiphuong2763@gmail.com>
2024-01-26 13:10:38 +0700

    feat: integrate with gif

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,4 +7,17 @@ document.addEventListener('scroll', function () {
     } else {
         hiddenElement.style.display = 'none'; // Hide the element
     }
+
+    // scroll to section 2 => visible aquaman_section_2, settimout 3s to hide
+
+    if (window.scrollY > window.innerHeight / 3) {
+        console.log(document.getElementById('aquaman_section_2'));
+        // Show aquaman section 2
+        document.getElementById('aquaman_section_2').style.display = 'block';
+
+        // Hide after 3 seconds
+        setTimeout(() => {
+            document.getElementById('aquaman_section_2').style.display = 'none';
+        }, 2800);
+    }
 });
