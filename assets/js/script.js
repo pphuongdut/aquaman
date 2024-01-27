2173a5ac191c7d44535e7442b8117844f8fc019e
Phuong Tran <tranphiphuong2763@gmail.com>
2024-01-27 15:01:55 +0700

    feat: update animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,3 @@
-// Function to check if an element is in the viewport
-function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <=
-            (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <=
-            (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
-
 document.addEventListener('scroll', function () {
     var hiddenElement = document.getElementById('bubbleContainer');
 
@@ -19,20 +6,5 @@ document.addEventListener('scroll', function () {
         hiddenElement.style.display = 'block'; // Show the element
     } else {
         hiddenElement.style.display = 'none'; // Hide the element
-    }
-
-    // if (window.scrollY > window.innerHeight / 3) {
-    //     var video = document.getElementById('aquaman_section_2');
-    //     video.play();
-    // }
-
-    const elementToAnimate = document.querySelector('.hidden-element');
-
-    if (isInViewport(elementToAnimate)) {
-        elementToAnimate.style.display = 'block';
-        elementToAnimate.classList.add(
-            'animate__animated',
-            'animate__slideInUp'
-        );
     }
 });
